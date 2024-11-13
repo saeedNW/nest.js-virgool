7398a78647fce58cd30565423b2ca9b346d13626
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-11-13 20:25:32 +0330

    Auth(CategoryModdule): Activate auth guard
    Description:
    Enables the auth guard for enhanced security, ensuring that only
    authenticated requests can access the module's endpoints.
    
    Logs:
    [Modified][AuthModule]: Exporting auth module services related to the
    auth guard.
    [Modified][CategoryModule]: Add auth and user modules to the main module
    file in order to prevent dependency errors.
    [Modified][CategoryModule]: Registering auth guard in madule controller
    file.

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CategoryService } from './category.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	UseGuards,
+} from "@nestjs/common";
+import { CategoryService } from "./category.service";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
+import { AuthGuard } from "../auth/guard/auth.guard";
 
-@Controller('category')
+@Controller("category")
+@UseGuards(AuthGuard)
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
 
-  @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
-  }
+	@Post()
+	create(@Body() createCategoryDto: CreateCategoryDto) {
+		return this.categoryService.create(createCategoryDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.categoryService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.categoryService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.categoryService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
-  }
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateCategoryDto: UpdateCategoryDto
+	) {
+		return this.categoryService.update(+id, updateCategoryDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.categoryService.remove(+id);
+	}
 }
--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -3,9 +3,11 @@ import { CategoryService } from "./category.service";
 import { CategoryController } from "./category.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CategoryEntity } from "./entities/category.entity";
+import { AuthModule } from "../auth/auth.module";
+import { UserModule } from "../user/user.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([CategoryEntity])],
+	imports: [AuthModule, UserModule, TypeOrmModule.forFeature([CategoryEntity])],
 	controllers: [CategoryController],
 	providers: [CategoryService],
 })
