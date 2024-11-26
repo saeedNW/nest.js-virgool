7cf6965e7cca6efed2bc9a3f39a3dc7d848be68a
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-11-26 17:29:36 +0330

    Refactor(Utilities): Rename string to array utility

--- src/modules/blog/dto/create-blog.dto.ts
+++ src/modules/blog/dto/create-blog.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Expose, Transform } from "class-transformer";
 import { IsArray, IsNotEmpty, IsNumberString, Length } from "class-validator";
-import { stringToArray } from "src/common/utils/string-to-array";
+import { stringToArray } from "src/common/utils/string-to-array.utils";
 
 export class CreateBlogDto {
 	@ApiProperty()
